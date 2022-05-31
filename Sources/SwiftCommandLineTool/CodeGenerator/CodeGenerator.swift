342918f2eab57fb9cac95d238b9f1abc08d3db73
Chiaote Ni <aaron.chiao.19@gmail.com>
2022-05-31 21:22:53 +0800

    3. make code generator

--- Sources/SwiftCommandLineTool/CodeGenerator/CodeGenerator.swift
+++ Sources/SwiftCommandLineTool/CodeGenerator/CodeGenerator.swift
@@ -20,11 +20,42 @@ final class CodeGenerator {
     }
 
     func generateExtensionCode(with datas: [LocalizationData]) {
+        var code = datas.reduce(into: Constants.sampler) { partialResult, data in
+            partialResult += """
+
+    var \(data.key): String { \"\(data.key)\".localization }
+"""
+        }
+        code += "\n}"
+
+        swiftFileExporter.exportSwiftFile(
+            with: code,
+            fileName: Constants.fileName
+        )
     }
 }
 
 extension CodeGenerator {
 
     enum Constants {
+
+        static var fileName: String { "String + Localization" }
+        static var variableName: String { "localization" }
+        static var sampler: String {
+"""
+//
+//  \(fileName).swift
+//
+
+import Foundation
+
+extension String {
+
+    var \(variableName): String {
+        NSLocalizedString(self, comment: "")
+    }
+
+"""
+        }
     }
 }
